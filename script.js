fdd799b5b7f9a4220ea7b940f587b6240d000823
Shekhar-2023 <126559347+Shekhar-2023@users.noreply.github.com>
2023-02-28 15:18:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+//document.getElementById("2").innerText="hhhhhh"
+//para1.innerText="hwo are you "+innerText
+let btn1= document.getElementById("btn1")
+btn1.addEventListener('click',function(e){
+    window.location="https://www.instagram.com/"
+
+})
+let btn2= document.getElementById("btn2")
+btn2.addEventListener('click',function(e){
+    window.location="https://www.youtube.com/"
+
+})
+let btn3= document.getElementById("btn3")
+btn3.addEventListener('click',function(e){
+    window.location="https://www.google.com/"
+
+})
+let btn4= document.getElementById("btn4")
+btn4.addEventListener('click',function(e){
+    window.location="https://www.facebook.com/"
+
+})
